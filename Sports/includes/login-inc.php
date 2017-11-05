b950e595d3ee80a06835db39081f463ac29768b7
vnand-csuf <32149105+vnand-csuf@users.noreply.github.com>
2017-11-04 17:03:10 -0700

    Updated with column names

--- Sports/includes/login-inc.php
+++ Sports/includes/login-inc.php
@@ -2,13 +2,21 @@
 
 session_start();
 
-//check if we clicked the Submit button
-if(isset($_POST['submit_btn'])){
+//check if we clicked the Login button
+if(isset($_POST['login-submit'])){
+
+	//testing code
+	//echo $_POST['uname'];
+	//exit();
+
+
 	//including database connection file
 	include '../dbconfig/config.php';
 
 	$uname = mysqli_real_escape_string($con, $_POST['uname']);
-	$pwd = mysqli_real_escape_string($con, $_POST['pwd']);
+	$pwd = mysqli_real_escape_string($con, $_POST['login_pwd']);
+
+
 
 	//Error handlers
 	//check if inputs are empty
@@ -17,7 +25,7 @@ if(isset($_POST['submit_btn'])){
 		exit();
 	} else{
 		// SQL query to check if user exists with the given username
-		$sql = "SELECT * FROM users WHERE username_column='$uname'";
+		$sql = "SELECT * FROM Users WHERE Email='$uname'";
 		$result = mysqli_query($con, $sql);
 		$resultCheck = mysqli_num_rows($result);
 
@@ -29,17 +37,30 @@ if(isset($_POST['submit_btn'])){
 			//insert data from query result in php array named row.
 			if($row = mysqli_fetch_assoc($result)){
 				//De-hashing the pasword
-				$hashedPwdCheck = password_verify($pwd, $row['password_column']);
+				$hashedPwdCheck = password_verify($pwd, $row['Password']);
 				//returns boolean
 				if(hashedPwdCheck == false){
 					header("Location: ../sports.php?login=error");
 					exit();
 				} elseif(hashedPwdCheck == true){
 					//Log in the user here, using Session variables
-					$_SESSION['u_id'] = $row['username_columnfrom DB'];
-					$_SESSION['fname'] = $row['fname_columnfrom DB'];
-					$_SESSION['email'] = $row['email_columnfrom DB'];
+					$_SESSION['u_id'] = $row['Email'];
+					$_SESSION['fname'] = $row['First_Name'];
+					$_SESSION['lname'] = $row['Last_Name'];
+					$_SESSION['gender'] = $row['Gender'];
+					$_SESSION['dob'] = $row['Dob'];
+					$_SESSION['city'] = $row['City'];
+					$_SESSION['state'] = $row['State'];
+					$_SESSION['zip'] = $row['ZIP'];
+					$_SESSION['phnum'] = $row['Phone_Number'];
+					$_SESSION['tennis'] = $row['Tennis'];
+					$_SESSION['bsk'] = $row['Basketball'];
+					$_SESSION['volley'] = $row['Volleyball'];
+					$_SESSION['hiking'] = $row['Hiking'];
+					$_SESSION['swim'] = $row['Swimming'];
+					$_SESSION['soccer'] = $row['Soccer'];
 					// storing values from successful query result into Session variables
+
 
 					header("Location: ../sports.php?login=success");
 					exit();
